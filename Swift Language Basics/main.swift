fc3c7170acd934d900b2d9395c8fe282a313a7f3
Tiara Jarrett <tiarajarrett@Tiaras-MacBook-Pro.local>
2018-01-28 18:42:00 -0600

    Initial Commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Tiara Jarrett on 1/28/18.
+//  Copyright © 2018 Tiara Jarrett. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
